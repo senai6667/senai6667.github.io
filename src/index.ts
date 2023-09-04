a36fb66f7d4971e2eac6b11d3ba16717a94bbded
senai6667 <2230283851@qq.com>
2023-09-04 18:55:19 +0800

    First file

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-//index.ts
-console.log("Hello, world!");
+import * as http from 'http';
+import * as path from 'path';
+
+comst server = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, 'public', 'index.html');
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(fs.readFileSync(filePath, 'utf-8'));
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
